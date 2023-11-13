f4df0d3015eccc98f9fd384183fe9df5305b9a30
Harsh Tripathi <harshtripathi@Harshs-MacBook-Air.local>
2023-11-13 17:57:41 +0530

    my new commit

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/CRUD/backendproj/settings.py
+sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/blogprojectdrf/backendproj/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     

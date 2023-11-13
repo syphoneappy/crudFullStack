80f63b761114b8f9a1770bc21e4db14f7235ae5a
Harsh Tripathi <harshtripathi@Harshs-MacBook-Air.local>
2023-11-14 02:18:49 +0530

    my commit

--- frontend/src/Components/Api.jsx
+++ frontend/src/Components/Api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const Api = axios.create({
-    baseURL:'http://127.0.0.1:8000/'
+    baseURL:"https://sea-lion-app-5eziv.ondigitalocean.app"
+    // baseURL:'http://127.0.0.1:8000/'
 });
 
 export default Api
